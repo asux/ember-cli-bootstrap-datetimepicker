9bf8727b452fc802f599e8e181481c9e55010a1f
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2017-02-24 14:49:39 +0200

    Replacing eonasdan-bootstrap-datetimepicker-npm to original

--- blueprints/ember-cli-bootstrap-datetimepicker/index.js
+++ blueprints/ember-cli-bootstrap-datetimepicker/index.js
@@ -10,7 +10,7 @@ module.exports = {
     */
     return this.addPackagesToProject([
       { name: 'ember-cli-moment-shim', target: '3.0.1' },
-      { name: 'eonasdan-bootstrap-datetimepicker-npm', target: '4.17.42' },
+      { name: 'eonasdan-bootstrap-datetimepicker', target: '^4.17.46' },
       { name: 'moment', target: '^2.17.1' },
       { name: 'moment-timezone', target: '^0.5.11' }
     ]);
