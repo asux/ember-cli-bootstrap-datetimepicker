f1bcbf1ad429af40cfab01060a0ea1f4d22f0f1f
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2017-08-30 13:36:06 +0300

    Target ember-cli-moment-shim to '^3.3.0'

--- blueprints/ember-cli-bootstrap-datetimepicker/index.js
+++ blueprints/ember-cli-bootstrap-datetimepicker/index.js
@@ -9,7 +9,7 @@ module.exports = {
       https://github.com/Eonasdan/bootstrap-datetimepicker/issues/1783
     */
     return this.addPackagesToProject([
-      { name: 'ember-cli-moment-shim', target: '3.3.0' },
+      { name: 'ember-cli-moment-shim', target: '^3.3.0' },
       { name: 'eonasdan-bootstrap-datetimepicker', target: '^4.17.47' },
       { name: 'moment', target: '^2.18.1' },
       { name: 'moment-timezone', target: '^0.5.13' }
